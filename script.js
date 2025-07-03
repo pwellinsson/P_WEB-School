bc06d8c81356d895a4fd15c016729679b6c21ce6
Daniel <barbosadaniel893@gmail.com>
2025-07-03 14:36:05 -0300

    Finalizando o script para exibir o h1 na tela

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /*Pegando o nome da pasta */
-function nomePasta(){
-    alert("Rodando")
+function nomePasta() {
     const inputT = document.getElementById('file')
-    const turma = document.getElementById('nomeTurma')
     const files = inputT.files
 
     if (files.length > 0){
@@ -11,17 +9,10 @@ function nomePasta(){
 
         if (relativePath){
             const nomePasta = relativePath.split('/')[0]
-            turma.innerHTML = nomePasta
-        }else{
-            turma.innerHTML ="Nenhuma pasta selecionada ou o arquivo foi encotrado"
+            localStorage.setItem('nomeDaTurma', nomePasta)  // salva no localStorage
         }
-    }else{
-        turma.innerHTML ="Nenhuma pasta selecionada ou o arquivo foi encotrado"
     }
 }
-    
-
-
 
 /*Criando o MODAL */
 const imagens = document.querySelectorAll('.galeria img')
