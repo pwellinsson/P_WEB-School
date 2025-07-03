092b6eaa545fe499fa42317728349051852429fb
Daniel <barbosadaniel893@gmail.com>
2025-07-03 13:54:46 -0300

    Criando o script para pegar o nome da pasta

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-const imagens = document.querySelectorAll('.galeria img')
-const modal = document.getElementById('modal')
-const modalImg = document.querySelector('#modal img')
-const closeImg = document.querySelector('#btnFechar img')
+const turma = document.getElementById('nomeTurma')
+const input_turma = document.getElementById('file')
+turma.innerHTML = input_turma
 
-imagens.forEach(imagem => {
-    imagem.addEventListener('click', () =>{
-        const imgSelect = imagem.src
-        modalImg.src = imgSelect
+console.log(turma)
 
-        modal.style.display = 'flex'
-        closeImg.style.display = 'block'
-    })
-})
 
-closeImg.addEventListener('click', ()=>{
-    modal.style.display = 'none'
-    closeImg.style.display = 'none'
-})
+// /*Criando o MODAL */
+// const imagens = document.querySelectorAll('.galeria img')
+// const modal = document.getElementById('modal')
+// const modalImg = document.querySelector('#modal img')
+// const closeImg = document.querySelector('#btnFechar img')
 
-modal.addEventListener('click', (event) => {
-    if (event.target == modal){
-        modal.style.display = 'none'
-        closeImg.style.display = 'none'
-    }
-})
+// imagens.forEach(imagem => {
+//     imagem.addEventListener('click', () =>{
+//         const imgSelect = imagem.src
+//         modalImg.src = imgSelect
+
+//         modal.style.display = 'flex'
+//         closeImg.style.display = 'block'
+//     })
+// })
+
+// closeImg.addEventListener('click', ()=>{
+//     modal.style.display = 'none'
+//     closeImg.style.display = 'none'
+// })
+
+// modal.addEventListener('click', (event) => {
+//     if (event.target == modal){
+//         modal.style.display = 'none'
+//         closeImg.style.display = 'none'
+//     }
+// })
