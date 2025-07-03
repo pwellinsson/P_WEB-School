b47a17481cb5aa94d16d80b916f2109c509021a7
Daniel <barbosadaniel893@gmail.com>
2025-07-03 14:18:30 -0300

    Continuando a função nomePasta - (não tá funcionando)

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-const turma = document.getElementById('nomeTurma')
-const input_turma = document.getElementById('file')
-turma.innerHTML = input_turma
+/*Pegando o nome da pasta */
+function nomePasta(){
+    alert("Rodando")
+    const inputT = document.getElementById('file')
+    const turma = document.getElementById('nomeTurma')
+    const files = inputT.files
 
-console.log(turma)
+    if (files.length > 0){
+        const firstFile = files[0]
+        const relativePath = firstFile.webkitRelativePath
+
+        if (relativePath){
+            const nomePasta = relativePath.split('/')[0]
+            turma.innerHTML = nomePasta
+        }else{
+            turma.innerHTML ="Nenhuma pasta selecionada ou o arquivo foi encotrado"
+        }
+    }else{
+        turma.innerHTML ="Nenhuma pasta selecionada ou o arquivo foi encotrado"
+    }
+}
+    
 
 
-// /*Criando o MODAL */
-// const imagens = document.querySelectorAll('.galeria img')
-// const modal = document.getElementById('modal')
-// const modalImg = document.querySelector('#modal img')
-// const closeImg = document.querySelector('#btnFechar img')
 
-// imagens.forEach(imagem => {
-//     imagem.addEventListener('click', () =>{
-//         const imgSelect = imagem.src
-//         modalImg.src = imgSelect
+/*Criando o MODAL */
+const imagens = document.querySelectorAll('.galeria img')
+const modal = document.getElementById('modal')
+const modalImg = document.querySelector('#modal img')
+const closeImg = document.querySelector('#btnFechar img')
 
-//         modal.style.display = 'flex'
-//         closeImg.style.display = 'block'
-//     })
-// })
+imagens.forEach(imagem => {
+    imagem.addEventListener('click', () =>{
+        const imgSelect = imagem.src
+        modalImg.src = imgSelect
 
-// closeImg.addEventListener('click', ()=>{
-//     modal.style.display = 'none'
-//     closeImg.style.display = 'none'
-// })
+        modal.style.display = 'flex'
+        closeImg.style.display = 'block'
+    })
+})
 
-// modal.addEventListener('click', (event) => {
-//     if (event.target == modal){
-//         modal.style.display = 'none'
-//         closeImg.style.display = 'none'
-//     }
-// })
+closeImg.addEventListener('click', ()=>{
+    modal.style.display = 'none'
+    closeImg.style.display = 'none'
+})
+
+modal.addEventListener('click', (event) => {
+    if (event.target == modal){
+        modal.style.display = 'none'
+        closeImg.style.display = 'none'
+    }
+})
